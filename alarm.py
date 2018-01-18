71abbe545514b265e5dafac86d5ff254f4c8f1a1
kjwon15 <kjwonmail@gmail.com>
2018-01-19 01:58:34 +0900

    Forecast with alarm

--- alarm.py
+++ alarm.py
@@ -22,7 +22,7 @@ YEELIGHT_HOST = 'http://tsubaki.lan:31337/'
 MIN_TEMP = 1700
 MAX_TEMP = 6500
 
-DURATION = 60 * 3 + 46
+DURATION = 60 * 3 + 46 - 20
 INTERVAL = 10
 
 logger = logging.getLogger(__name__)
@@ -86,23 +86,46 @@ def load_playlist():
 def do_light_stuff():
     # Light time!
     logger.info('yeelight time')
-    session.put(YEELIGHT_HOST + 'switch')
+    try:
+        session.put(YEELIGHT_HOST + 'switch', timeout=5)
+    except Exception as e:
+        logger.error(e)
+        logger.warning('Cannot on yeelight')
+    else:
+        time.sleep(1)
     scheduler = sched.scheduler(time.time, time.sleep)
     steps = DURATION // INTERVAL
 
     def set_light(**kwargs):
         logger.info('Set light {}'.format(kwargs))
-        session.post(
-            YEELIGHT_HOST + 'light',
-            data=kwargs
-        )
+        try:
+            session.post(
+                YEELIGHT_HOST + 'light',
+                data=kwargs,
+                timeout=5
+            )
+        except Exception as e:
+            logger.error(e)
+            logger.warning('Cannot controll yeelight')
 
     def turn_off_yeelight():
-        session.delete(YEELIGHT_HOST + 'switch')
+        try:
+            session.post(
+                YEELIGHT_HOST + 'light',
+                data={
+                    'brightness': 40,
+                    'rgb': 'ff0000',
+                },
+                timeout=5
+            )
+            session.delete(YEELIGHT_HOST + 'switch')
+        except Exception as e:
+            logger.warning('Cannot off yeelight')
+            logger.error(e)
 
     for step in range(steps):
         temp = int(MIN_TEMP + ((MAX_TEMP - MIN_TEMP) * (step / steps)))
-        brightness = int(100 * step / steps)
+        brightness = int(100 * step / steps) or 1
         scheduler.enter(step * INTERVAL, 1, set_light, kwargs={
             'temp': temp,
             'brightness': brightness,
@@ -112,9 +135,44 @@ def do_light_stuff():
     scheduler.run()
 
 
+def forecast():
+    logger.info('forecast')
+    try:
+        resp = session.get(
+            'https://query.yahooapis.com/v1/public/yql', params={
+                'format': 'json',
+                'q': ("select item.forecast from weather.forecast(1) where u='c' and"
+                      " woeid in (select woeid from geo.places(1) where text = 'daejeon')"),
+            }
+        )
+
+        forecast = resp.json()['query']['results']['channel']['item']['forecast']
+    except Exception as e:
+        print(resp.json())
+        logger.error('Failed to get forecast: {}'.format(e))
+        return
+
+    try:
+        msg = (
+            "Today's forecast is {text}."
+            " Highest temperature is {high} degrees"
+            " and lowest temperature is {low} degrees."
+        ).format(text=forecast['text'], high=forecast['high'], low=forecast['low'])
+        session.post(
+            'http://tsubaki.lan:1775/tts', {
+                'msg': msg,
+                'voiceid': 'Amy',
+            }
+        )
+    except Exception as e:
+        logger.error('Failed to request TTS: {}'.format(e))
+        return
+
+
 if __name__ == '__main__':
 
     load_playlist()
     do_light_stuff()
     turn_on_light()
+    forecast()
     logger.info('done')
